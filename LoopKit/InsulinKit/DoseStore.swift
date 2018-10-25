4cabd301cea79cfc79f6d9b0468d8126c76e683b
brian.wang <brian.wang@avepoint.com>
2018-10-25 22:56:16 +0800

    代码降级到2.1.2

--- LoopKit/InsulinKit/DoseStore.swift
+++ LoopKit/InsulinKit/DoseStore.swift
@@ -1240,24 +1240,17 @@ extension DoseStore {
                     report.append("")
                     report.append("### getPumpEventValues")
 
-                    var firstPumpEventDate = self.cacheStartDate
-
                     switch result {
                     case .failure(let error):
                         report.append("Error: \(error)")
                     case .success(let values):
                         report.append("")
-
-                        if let firstEvent = values.last {
-                            firstPumpEventDate = firstEvent.date
-                        }
-
                         for value in values {
                             report.append("* \(value)")
                         }
                     }
 
-                    self.getNormalizedDoseEntries(start: firstPumpEventDate) { (result) in
+                    self.getNormalizedDoseEntries(start: Date.distantPast) { (result) in
                         report.append("")
                         report.append("### getNormalizedDoseEntries")
 
@@ -1271,7 +1264,7 @@ extension DoseStore {
                             }
                         }
 
-                        self.getNormalizedPumpEventDoseEntriesOverlaidWithBasalEntries(after: firstPumpEventDate, completion: { (result) in
+                        self.getNormalizedPumpEventDoseEntriesOverlaidWithBasalEntries(after: Date.distantPast, completion: { (result) in
 
                             report.append("")
                             report.append("### getNormalizedPumpEventDoseEntriesOverlaidWithBasalEntries")
